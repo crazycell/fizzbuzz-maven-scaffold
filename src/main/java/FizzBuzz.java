5772058a64c5f39125629226151bdbfecac6b773
ren <781668049@qq.com>
2020-01-15 23:37:01 +0800

    update~

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -3,11 +3,13 @@ import org.apache.commons.lang3.StringUtils;
 public class FizzBuzz {
     public String print(int n) {
         String result = "";
-        if(n % 3 == 0) {
+        if (n % 3 == 0) {
             result += "fizz";
-        }if(n % 5 == 0) {
+        }
+        if (n % 5 == 0) {
             result += "buzz";
-        }if(StringUtils.isEmpty(result)) {
+        }
+        if (StringUtils.isEmpty(result)) {
             result += n;
         }
         return result;
